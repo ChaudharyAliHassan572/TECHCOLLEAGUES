d3f9a75a412655c23ab0fd8ba635ffea6bec62e6
chaud <chaud@KING>
2025-08-29 10:23:01 +0500

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿var name=personName.parse("ALi");
+name.name("Hassan");
+class personName
+{
+    private String FirstName;
+
+    public void name(String LastName)
+    {
+        Console.WriteLine("Name: {0} {1}",FirstName,LastName);
+    }
+    public static personName parse(String name)
+    {
+        var personName=new personName();
+        personName.FirstName=name;
+        return personName;
+    }
+}
